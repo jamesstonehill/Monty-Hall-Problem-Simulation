5c7da36731098ebae45cf2149ddd2cd6c3f188c1
James Stonehill <jamesstonehill@gmail.com>
2016-10-01 21:21:27 +0100

    test setup

---
+++ jasmine/spec/MH_ClassSpec.js
@@ -0,0 +1,21 @@
+describe("MontyHallSimulation", function() {
+  var simulationTen;
+
+  beforeEach(function() {
+    simulationTen = new MontyHallSimulation(10);
+  });
+
+  describe('.pickFirstDoor',function() {
+    it("sets the specified door to have isFirstSelection equal true", function() {
+      
+
+    });
+
+  });
+
+  describe('.pickSecondDoor',function() {
+
+
+  });
+
+});
